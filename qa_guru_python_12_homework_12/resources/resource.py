9d9126aa751d9d04c11f86eb773e3a44540ebd83
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-04-25 10:13:50 +0300

    for review

---
+++ qa_guru_python_12_homework_12/resources/resource.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Resource:
+
+    def path(self, file):
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(curr_dir, file)
+
+
+resource = Resource()
---
+++ tests/test_demoqa.py
@@ -0,0 +1,40 @@
+from qa_guru_python_12_homework_12.model.registration_page import RegistrationPage
+import allure
+
+
+@allure.title("Successful fill form")
+def test_complete_todo(setup_browser):
+    with allure.step("Open registrations form"):
+        registration_page = RegistrationPage()
+        registration_page.open()
+
+    with allure.step("Fill form"):
+        registration_page.fill_first_name('Ivan')
+        registration_page.fill_last_name('Durian')
+        registration_page.fill_email('ID@gmail.com')
+        registration_page.fill_gender('Other')
+        registration_page.fill_mobile('1234512345')
+        registration_page.fill_date_of_birth('1900', 'May', '10')
+        registration_page.fill_subjects('Computer Science')
+        registration_page.fill_hobbies('Music')
+        registration_page.fill_picture('clover.jpg')
+        registration_page.fill_current_address('Thailand, Phuket')
+        registration_page.fill_state('Haryana')
+        registration_page.fill_city('Panipat')
+        registration_page.submit()
+
+    with allure.step("Check form results"):
+        registration_page.should_registered_user_with(
+            'Ivan',
+            'Durian',
+            'ID@gmail.com',
+            'Other',
+            '1234512345',
+            '10 May,1900',
+            'Computer Science',
+            'Music',
+            'clover.jpg',
+            'Thailand, Phuket',
+            'Haryana',
+            'Panipat'
+        )
